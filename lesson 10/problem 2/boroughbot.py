06eea60124269dd0aa76649ae16447ebdec7632c
cespinozaactvf <44033422+cespinozaactvf@users.noreply.github.com>
2019-01-19 11:02:31 -0500

    Add files via upload

---
+++ lesson 10/problem 2/boroughbot.py
@@ -0,0 +1,19 @@
+print('_' * 65)
+
+print('Best Borough Bot:')
+
+print()
+
+borough = input('What borough are you from? ')
+
+if borough == 'Queens':
+	print('Nice, Queens is the best borough!')
+
+else:
+	print('Too bad, Queens is the best!')
+	print('Obviously.')
+	print('But really Brooklyn is the best.')
+	print('Cary just thinks Brooklyn is the best since he thinks his opinion matters.')
+	print('His opinion actually matter, just not when its comes to borough.')
+
+print('_' * 65)
